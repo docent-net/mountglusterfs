eaae82a69df62a64f2eb14ca3b179b4827a46343
Maciej Lasyk <docent.net@gmail.com>
2013-09-16 16:13:57 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 mountglusterfs
 ==============
 
-Init scripts (Debian LSB &amp;&amp; RHEL SysVinit)
+Init scripts (Debian LSB && RHEL SysVinit).
+
+Those scripts provide scripts for mounting GlusterFS based drives in proper 
+order during startup. For example having gluster server on same machine where
+mounting provided by this server mountpoints it's impossible to mount it with
+_netdev option from fstab because by default mountnfs is started before 
+gluster-server service.
+
+With below scripts You can set new system service, which mounts all glusterfs
+partitions entered in /etc/fstab with _netdev option.
+
+Installation:
+--------------
+**Debian**: copy debian/mountglusterfs.sh to /etc/init.d and run command:
+*update-rc.d mountglusterfs.sh defaults*
+
+After that you can use mountglusterfs keyword in "Required start:" option of 
+choosen LSB scripts. For more informations go to:
+
+- LSB scripts: https://wiki.debian.org/LSBInitScripts
+- Dependency based boot: https://wiki.debian.org/LSBInitScripts/DependencyBasedBoot
+
+Usage:
+--------------
+After installing create new /etc/fstab records using 'glusterfs' as fstype and
+_netdev as additional mount option. After rebooting those resources should be 
+mounted in choosen order.
+
+Todo:
+--------------
+- RHEL & CentOS scripts
---
+++ debian/mountglusterfs.sh
@@ -0,0 +1,88 @@
+#! /bin/bash
+### BEGIN INIT INFO
+# Provides:          mountglusterfs
+# Required-Start:    $local_fs $network glusterfs-server
+# Required-Stop:     $local_fs $network glusterfs-server
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Wait for glusterfs-server before mounting gluster drives
+# Description:       When system services rely on glusterfs drives
+#                    than those services' LSB scripts should have certainty
+#                    that glusterfs based drives are up and running.
+### END INIT INFO
+date +%s_%N > /tmp/glusterlog.6.mountglusterfs.time
+. /lib/init/vars.sh
+. /lib/init/mount-functions.sh
+. /lib/lsb/init-functions
+
+do_wait_sync_mount() {
+	GLUSTER_DEVS=()
+	GLUSTER_MTPTS=()
+	GLUSTER_OPTS=()
+	# Read through fstab line by line. If it is glusterfs and _netdev, set the 
+	# flag for mounting glusterfs file systems.	
+	for file in "$(eval ls $(fstab_files))"; do
+		if [ -f "$file" ]; then
+			while read DEV MTPT FSTYPE OPTS REST; do
+				F_OPT=0
+				F_FSTYPE=0
+				case "$OPTS" in
+				_netdev)
+					F_OPT=1
+					;;
+				esac
+				case "$FSTYPE" in
+				glusterfs)
+					F_FSTYPE=1
+					;;
+				esac
+				
+				if [ "$F_OPT" -eq "1" -a "$F_FSTYPE" -eq "1" ]; then
+					GLUSTER_DEVS+=($DEV)
+					GLUSTER_MTPTS+=($MTPT)
+					GLUSTER_OPTS+=($OPTS)
+				fi
+			done < "$file"			
+		fi
+	done
+	
+	# Wait for each path, the timeout is for all of them as that's
+	# really the maximum time we have to wait anyway
+	TIMEOUT=900
+	for ((i = 0; i <=  ${#GLUSTER_DEVS[@]}-1; i++)); do
+		log_action_begin_msg "Mounting ${GLUSTER_DEVS[$i]} at " \
+		"${GLUSTER_MTPTS[$i]}"
+
+		while ! mountpoint -q ${GLUSTER_MTPTS[$i]}; do
+			sleep 0.1
+			mount -t glusterfs -o ${GLUSTER_OPTS[$i]} ${GLUSTER_DEVS[@]} ${GLUSTER_MTPTS[$i]}
+			
+			TIMEOUT=$(( $TIMEOUT - 1 ))
+			if [ $TIMEOUT -le 0 ]; then
+				log_action_end_msg 1
+				break
+			fi			
+		done
+		
+		if [ $TIMEOUT -gt 0 ]; then
+			log_action_end_msg 0
+		fi
+	done
+}
+
+case "$1" in
+    start)
+		do_wait_sync_mount;;
+    restart|reload|force-reload)
+        echo "Error: argument '$1' not supported" >&2
+        exit 3
+        ;;
+    stop)
+        ;;
+    *)
+        echo "Usage: $0 start|stop" >&2
+        exit 3
+        ;;
+esac
+
+: exit 0
